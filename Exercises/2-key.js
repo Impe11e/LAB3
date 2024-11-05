1895692abbee8fc3784e67232911c4c3bdac18c2
ariia horunzha <1mpellee@gmail.com>
2024-11-06 00:02:43 +0200

    first commit

--- Exercises/2-key.js
+++ Exercises/2-key.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const generateKey = (length, possible) => {
-  // Generate string of random characters
-  // Use Math.random() and Math.floor()
-  // See documentation at MDN
+  const bord = possible.length;
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const char = Math.floor(Math.random() * bord);
+    key += possible[char];
+  }
+  return key;
 };
 
 module.exports = { generateKey };
